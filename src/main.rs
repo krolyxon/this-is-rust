bb6ee73d3d486109704f20e59d568581ec64ec6b
krolyxon <krolyxon@tutanota.com>
2022-09-15 13:27:37 +0530

    tryna talk to a god

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("This is Rust.");
+    let mut input = String::new();
+
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+
+    match input.trim() {
+        "yes" => {
+            println!("yes, you are right. this is rust.");
+        }
+        _ => {
+            println!("No, you wrong.");
+        }
+    }
 }
